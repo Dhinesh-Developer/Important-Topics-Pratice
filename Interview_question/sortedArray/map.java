611c18f831c9bea6a2aa56b078f38d71e7b027d7
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-09-22 19:40:39 +0530

    Add files via upload

---
+++ Interview_question/sortedArray/map.java
@@ -0,0 +1,19 @@
+package sorted_Array;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class map {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+	    HashMap<String,Integer> map = new HashMap<String, Integer>();
+	    
+	    map.put("India",1);
+	    map.put("Japan", 2);
+	    System.out.println(map);
+	}
+
+}
